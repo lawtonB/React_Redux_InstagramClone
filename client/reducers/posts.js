492fa9993ae9f08d71c297953663fd20c8b5fcc1
lawtonB <lawton.a.browning@gmail.com>
2016-08-15 12:54:58 -0700

    add update likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -2,10 +2,16 @@
 
 //set state to empty array for first time
 export default function posts(state=[], action) {
-  
+  switch(action.type){
+    case 'INCREMENT_LIKES' :
+    const i = action.index;
+    return [
+      ...state.slice(0,i), //before updated post
+      {...state[i], likes: state[i].likes + 1},
+      ...state.slice(i + 1), //after updated post
 
-  console.log('the post will change')
-  // console.log(state, action);
-  return state;
-
+    ]
+    default:
+    return state
+  }
 }
